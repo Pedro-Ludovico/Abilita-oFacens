b6e157a0332dfc9056fdeffc0dd2bcc37d8ac288
Pedro-Ludovico <103973593+Pedro-Ludovico@users.noreply.github.com>
2022-04-26 22:48:08 -0300

    Update Abilitação.java

--- Abilitação.java
+++ Abilitação.java
@@ -2,25 +2,27 @@ package ex1;
 
 import java.util.Scanner;
 
-public class Abilitação {
+public class AbilitaÃ§Ã£o {
 
 	public static void main(String[]args) {
-		Scanner ler = new Scanner(System.in);
+		Scanner ler = new Scanner(System.in); //Metodo scanner
 		int psi, leg, dir;
 		
-		System.out.println("Digite 1 para sim e 2 para não");
-		System.out.println("Você foi aprovado no exame Psicotecnico:");
-		psi = ler.nextInt();
-		System.out.println("Você foi aprovado no exame de Legislação:");
-		leg = ler.nextInt();
-		System.out.println("Você foi aprovado no exame de direção:");
-		dir = ler.nextInt();
+		System.out.println("Digite 1 para sim e 2 para nÃ£o");
+		System.out.println("VocÃª foi aprovado no exame Psicotecnico:");
+		psi = ler.nextInt(); //pegando resposta 1
+		System.out.println("VocÃª foi aprovado no exame de LegislaÃ§Ã£o:");
+		leg = ler.nextInt(); //pegando resposta 2
+		System.out.println("VocÃª foi aprovado no exame de direÃ§Ã£o:");
+		dir = ler.nextInt(); //pegando resposta 3
 		
 		if(psi == 1 && leg == 1 && dir == 1) 
 		{
-			System.out.println("Você pode sim guiar um veiculo");
+			//caso todas as alternativas estejam marcadas como sim executar
+			System.out.println("VocÃª pode sim guiar um veiculo");
 		}else {
-			System.out.println("Você não pode guiar um veiculo ainda");
+			//caso alguma das alternativas estiver marcada como nÃ£o
+			System.out.println("VocÃª nÃ£o pode guiar um veiculo ainda");
 		}
 	}
 }
